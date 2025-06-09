19260893a0584a2684b1f015d798d2eed5539233
ecrowder05 <ecrowder@student.uaustin.org>
2025-06-09 13:04:29 -0500

    Update MyEscrow.sol

--- escrow/MyEscrow.sol
+++ escrow/MyEscrow.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MyEscrow {
 
     // Replace this address with the token you created (or whatever token you want to use).
-    address constant tokenToEscrow = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270 ;
+    address constant tokenToEscrow = 0x455e53CBB86018Ac2B8092FdCd39d8444aFFC3F6 ;
     // This tells the contract that the address you provided is a token that can be transferred.
     IERC20 escrowToken = IERC20(tokenToEscrow); 
     address public buyerAddress = address(0);
